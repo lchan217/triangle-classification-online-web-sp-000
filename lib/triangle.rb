4304f00f9d043548fc76b93e7a6e35d07cbef8a1
Lizzie Chan <lchan217@gmail.com>
2019-02-06 15:52:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,14 @@ class Triangle
     @side3 = side3
   end 
   def kind 
-    if 
+    if @side1<0 || @side2<0 || @side3<0
       begin
         raise TriangleError
       end
+    elsif @side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2
+      begin 
+        raise TriangleError
+      end 
     end
     
     if @side1 == @side2 && @side2 == @side3 && @side1 == @side3
