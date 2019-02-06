0f52bb36ce8a9eb4a852431f50613a1f1f89bad5
Lizzie Chan <lchan217@gmail.com>
2019-02-06 15:41:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       :isosceles
     elsif @side1 != @side2 || @side2 != @side3 || @side1 != @side3
       :scalene
-    else 
+    else (@side1<0 || @side2<0 || @side3<0) || (@side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2)
       begin
           raise TriangleError
         rescue TriangleError => error
@@ -23,7 +23,6 @@ class Triangle
   
   class TriangleError < StandardError
     def message
-      if (@side1<0 || @side2<0 || @side3<0) || (@side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2)
         "invalid"
       end
     end
