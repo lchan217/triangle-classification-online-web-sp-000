9fa31ba5de453bcd7fe3f2c7170fc7baf33613bd
Lizzie Chan <lchan217@gmail.com>
2019-02-06 15:40:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  def kind 
+    if @side1 == @side2 && @side2 == @side3 && @side1 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2==@side3 || @side1==@side3
+      :isosceles
+    elsif @side1 != @side2 || @side2 != @side3 || @side1 != @side3
+      :scalene
+    else 
+      begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+      end
+    end
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      if (@side1<0 || @side2<0 || @side3<0) || (@side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2)
+        "invalid"
+      end
+    end
+  end
+
 end
