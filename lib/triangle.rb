aae563636e2eefedbccf160b56e6f77071c2b054
Lizzie Chan <lchan217@gmail.com>
2019-02-06 15:52:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,9 +27,7 @@ class Triangle
   
   class TriangleError < StandardError
     def message
-      if (@side1<0 || @side2<0 || @side3<0) || (@side1+@side2<@side3 || @side2+@side3<@side1 || @side1+@side3<@side2)
-        "invalid"
-      end
+      "invalid"
     end
   end
 
